50813bbeb88c2f1b2eb27c22feb1d1b1673d214b
tiagogb <tgbmateriais@gmail.com>
2025-03-18 19:20:07 -0300

    feature: implement structure for candidate

--- src/main/java/br/com/tiagogarbi/gestao_vagas/modules/candidate/useCases/ListAllJobsByFilterUseCase.java
+++ src/main/java/br/com/tiagogarbi/gestao_vagas/modules/candidate/useCases/ListAllJobsByFilterUseCase.java
@@ -1,5 +1,20 @@
 package br.com.tiagogarbi.gestao_vagas.modules.candidate.useCases;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.tiagogarbi.gestao_vagas.modules.company.entities.JobEntity;
+import br.com.tiagogarbi.gestao_vagas.modules.company.repositories.JobRepository;
+
+@Service
 public class ListAllJobsByFilterUseCase {
-  
+
+  @Autowired
+  private JobRepository jobRepository;
+
+  public List<JobEntity> execute(String filter) {
+    return this.jobRepository.findByDescriptionContainingIgnoreCase(filter);
+  }
 }
