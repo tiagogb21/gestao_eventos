b7759af2cb8d18057f3cae036d7cf2207d120504
tiagogb <tgbmateriais@gmail.com>
2025-03-18 18:36:26 -0300

    feature: include more data in candidate entity

--- src/main/java/br/com/tiagogarbi/gestao_vagas/modules/candidate/CandidateEntity.java
+++ src/main/java/br/com/tiagogarbi/gestao_vagas/modules/candidate/CandidateEntity.java
@@ -4,14 +4,26 @@ import java.util.UUID;
 
 import org.hibernate.validator.constraints.Length;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.media.Schema.RequiredMode;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
 @Data
+@Entity(name="candidate")
 public class CandidateEntity {
+  @Id
+  @GeneratedValue(strategy=GenerationType.UUID)
   private UUID id;
+
+  @Schema(example="John Doe", requiredMode=RequiredMode.REQUIRED)
   private String name;
+
   @Pattern(regexp="^(?!\\s*$).*", message="O campo [username] não pode conter espaços em branco")
   private String username;
   @Email(message="O campo [email] deve conter um e-mail válido")
