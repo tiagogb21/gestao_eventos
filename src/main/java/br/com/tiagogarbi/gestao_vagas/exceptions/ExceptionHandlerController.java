ed483751c11d8477c45024b60ed093e3f26599ef
tiagogb <tgbmateriais@gmail.com>
2025-03-18 11:49:31 -0300

    feature: imcrements exceptions

--- src/main/java/br/com/tiagogarbi/gestao_vagas/exceptions/ExceptionHandlerController.java
+++ src/main/java/br/com/tiagogarbi/gestao_vagas/exceptions/ExceptionHandlerController.java
@@ -1,19 +1,36 @@
 package br.com.tiagogarbi.gestao_vagas.exceptions;
-
-import org.springframework.context.MessageSource;
-import org.springframework.context.i18n.LocaleContextHolder;
-import org.springframework.web.bind.MethodArgumentNotValidException;
-import org.springframework.web.bind.annotation.ControllerAdvice;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-
-@ControllerAdvice
-public class ExceptionHandlerController {
-  private MessageSource messageSource;
-
-  @ExceptionHandler(MethodArgumentNotValidException.class)
-  public void handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
-    e.getBindingResult().getAllErrors().forEach(err -> {
-      String message = messageSource.getMessage(err, LocaleContextHolder.getLocale());
-    });
-  }
-}
+ 
+ import java.util.ArrayList;
+ import java.util.List;
+ 
+ import org.springframework.context.MessageSource;
+ import org.springframework.context.i18n.LocaleContextHolder;
+ import org.springframework.http.HttpStatus;
+ import org.springframework.http.ResponseEntity;
+ import org.springframework.web.bind.MethodArgumentNotValidException;
+ import org.springframework.web.bind.annotation.ControllerAdvice;
+ import org.springframework.web.bind.annotation.ExceptionHandler;
+ 
+ @ControllerAdvice
+ public class ExceptionHandlerController {
+ 
+   private final MessageSource messageSource;
+ 
+   public ExceptionHandlerController(MessageSource messageSource) {
+     this.messageSource = messageSource;
+   }
+ 
+   @ExceptionHandler(MethodArgumentNotValidException.class)
+   public ResponseEntity<List<ErrorMessageDTO>> handleMethodArgumentNotValidException(
+       MethodArgumentNotValidException e) {
+     List<ErrorMessageDTO> dto = new ArrayList<>();
+ 
+     e.getBindingResult().getFieldErrors().forEach(err -> {
+       String message = messageSource.getMessage(err, LocaleContextHolder.getLocale());
+       ErrorMessageDTO error = new ErrorMessageDTO(message, err.getField());
+       dto.add(error);
+     });
+ 
+     return new ResponseEntity<>(dto, HttpStatus.BAD_REQUEST);
+   }
+ }
